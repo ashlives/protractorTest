4f102cafcefc5bd77c2a38e3e13badfc44ab653c
Ashish Deshmukh <ashish.p.deshmukh@gmail.com>
2017-07-07 13:36:01 +0530

    Initial commit

---
+++ conf.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Ashish on 07-07-2017.
+ */
+exports.config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js']
+}
---
+++ spec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Ashish on 07-07-2017.
+ */
+
+describe('Protractor Testing', function() {
+    it('should open google page', function() {
+        browser.ignoreSynchronization=true; // This is to be added if your application is non-angular
+        browser.get('http://www.google.com');
+        expect(browser.getTitle()).toEqual('Google');
+        element(by.css('input.gsfi')).sendKeys('Hello testing')
+    });
+});
