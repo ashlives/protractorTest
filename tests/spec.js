0bdb6c9fbb77abc9e3a1a9fb8e0d7f6cbe55177c
Ashish Deshmukh <ashish.p.deshmukh@gmail.com>
2017-07-08 17:21:50 +0530

    Moved specs file to tests folder

--- tests/spec.js
+++ tests/spec.js
@@ -7,6 +7,10 @@ describe('Protractor Testing', function() {
         browser.ignoreSynchronization=true; // This is to be added if your application is non-angular
         browser.get('http://www.google.com');
         expect(browser.getTitle()).toEqual('Google');
+        //element(by.css('input.gsfi')).sendKeys('Hello testing')
+    });
+
+    it('will search for keyword: Hello Testing', function () {
         element(by.css('input.gsfi')).sendKeys('Hello testing')
     });
 });
@@ -15,5 +19,12 @@ describe('Protractor Testing 2', function(){
     it('should open the linkedin page', function() {
         browser.get('http://www.linkedin.com');
         expect(browser.getTitle()).toEqual('LinkedIn: Log In or Sign Up');
+    });
+});
+
+describe('shit test', function () {
+    it('should open facebook page', function () {
+        browser.get('http://facebook.com');
+        expect(browser.getTitle()).toEqual('Facebook – log in or sign up');
     });
 });
