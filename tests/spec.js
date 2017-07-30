a54b0fbfd0560a76f22b2e401f8559f38ef431b7
Ashish <ashish.p.deshmukh@gmail.com>
2017-07-30 14:29:19 +0530

    Added BeforeEach in specs

--- tests/spec.js
+++ tests/spec.js
@@ -1,11 +1,12 @@
 describe('Testing Viu', function() {
-    it('should have a title', function() {
+    beforeEach(function () {
         browser.get('https://viu.com');
-        expect(browser.getTitle()).toEqual('Viu - Watch TV Shows & Movies Online');
     });
-});
+    
+    it('should have a title', function() {
+        expect(browser.getTitle()).toContain('Viu');
+    });
 
-describe('Choosing Hindi option', function () {
     it('should select Hindi option', function () {
         element(by.xpath('//*[@id="ngdialog1"]/div/div[1]/div/div[1]')).click();
         element(by.css('[ng-click="saveSelection()"]')).click();
--- tests/teluguselection.spec.js
+++ tests/teluguselection.spec.js
@@ -1,16 +1,15 @@
-
 describe('Testing Viu', function() {
-    it('should have a title', function() {
+    beforeEach(function () {
         browser.get('https://viu.com');
-        expect(browser.getTitle()).toEqual('Viu - Watch TV Shows & Movies Online');
     });
-});
 
-describe('Selecting Telugu Language', function () {
+    it('should have a title', function() {
+        expect(browser.getTitle()).toContain('Viu');
+    });
+
     it('Should select Telugu language', function () {
         element(by.xpath('.//*[@id="ngdialog1"]/div/div[1]/div/div[2]')).click();
         element(by.css('[ng-click="saveSelection()"]')).click();
         expect(browser.getCurrentUrl()).toContain('in-telugu');
-    })
-
+    });
 });
